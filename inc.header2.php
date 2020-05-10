753f1ff24c3a4ad60a6d1886cc5910c762fac7e5
Steve Dibb <steve.dibb@gmail.com>
2020-05-09 23:45:01 -0600

    Add JavaScript I don't remember pulling in

--- inc.header2.php
+++ inc.header2.php
@@ -67,3 +67,22 @@
 // 	}
 
 ?>
+
+<script src="https://www.google.com/jsapi?key=ABQIAAAAyI6LxePEcQ0oNX5IX2K4YhTvJdjsJxEPNHJyN_PXMNSvZuQykBR54JHiiczDtI81rAIApKrO40k94A" type="text/javascript"></script>
+<script type='text/javascript'>
+google.load("prototype", "1.6.1.0");
+function disableAutocomplete() { if($('searchForm')) { $('searchForm').writeAttribute('autocomplete', 'off'); }}
+var _gaq = _gaq || [];
+ _gaq.push(['_setAccount', 'UA-24554315-1']);
+ _gaq.push(['_setDomainName', '.znurt.org']);
+ _gaq.push(['_trackPageview']);
+
+ (function() {
+   var ga = document.createElement('script'); ga.type =
+'text/javascript'; ga.async = true;
+   ga.src = ('https:' == document.location.protocol ? 'https://ssl' :
+'http://www') + '.google-analytics.com/ga.js';
+   var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(ga, s);
+ })();
+</script>
